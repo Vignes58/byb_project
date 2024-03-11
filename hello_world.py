e6eee8b272b9bc91ca60e873e53331bfc299b5c8
Vignes58 <154813432+Vignes58@users.noreply.github.com>
2024-03-11 20:10:27 +0000

    Update helloWorld.py to print 'Git is Awesome!'

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+# the user has been promted to enter his/her name and age 
+
+# assigned a variable called name and age
+
+name = input("enter your name:")
+print (name) 
+age = input("enter your age:")
+
+#print (name + " " + "your age is " + " " +  age)
+
+print (f"hello {name} your age is {age}")
+print ("hello world!") # prints hello world 
+
+
+
+
